c9d33e34d44515a0bda70eb261d634f4a985609a
Joel Rosko <joelrosko@MacBook-Pro-som-tillhor-Joel.local>
2024-10-03 09:40:14 -0700

    Group create endpoint added

--- api/src/controllers/groups_controller.py
+++ api/src/controllers/groups_controller.py
@@ -1,6 +1,6 @@
 from flask import Response, Blueprint, jsonify, request
 
-from src.services.deconz_service import get_from_deconz, put_to_deconz
+from src.services.deconz_service import get_from_deconz, put_to_deconz, post_to_deconz
 
 groups = Blueprint("groups", __name__)
 
@@ -23,5 +23,33 @@ async def get_group(group_id):
         endpoint = f"/groups/{group_id}"
         data = await get_from_deconz(endpoint=endpoint)
         return jsonify(data)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+# Route to create group
+# /api/v1/groups/create
+@groups.route("/create", methods = ["POST"])
+async def create_group():
+    try:
+        try:
+            body = request.get_json()
+        except Exception:
+            return jsonify({"error": "no body data included"}), 400
+        
+        if "name" not in body:
+            return jsonify({"error": "'name' state must be provided"}), 400
+        
+        group_name = body['name']
+
+        if len(group_name) <= 0 or len(group_name) > 32:
+            return jsonify({"error": "'name' must be between 1 - 32 chars"}), 400
+        
+        endpoint = f'/groups'
+        payload = {"name": group_name}
+
+        response = await post_to_deconz(endpoint, payload)
+
+        return jsonify(response)
+        
     except Exception as e:
         return jsonify({"error": str(e)}), 500
