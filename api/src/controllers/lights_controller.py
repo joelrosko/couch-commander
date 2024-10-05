e4365e00721ad743e49bce8304618fd67a60126c
joelrosko <joel.rosko97@gmail.com>
2024-10-05 22:50:24 +0200

    Endpoint to find new devices (#29)

--- api/src/controllers/lights_controller.py
+++ api/src/controllers/lights_controller.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from aiohttp import ClientResponseError
 
-from src.services.deconz_service import get_from_deconz
+from src.services.deconz_service import get_from_deconz, put_to_deconz
 from src.utils.response_util import build_response
 
 lights = Blueprint("lights", __name__)
@@ -13,6 +13,20 @@ async def get_lights():
     try:
         endpoint = "/lights"
         response = await get_from_deconz(endpoint=endpoint)
+        return build_response(data=response, status=200)
+    except ClientResponseError as e:
+        return build_response(error=f"Failed at: {e.message}", status=e.status)
+    except Exception as e:
+        return build_response(error="Server error", status=500)
+
+# Route to add new lights
+# /api/v1/lights/add
+@lights.route("/add", methods = ["PUT"])
+async def get_lights():
+    try:
+        endpoint = "/config"
+        payload = {"permitjoin": 60}
+        response = await put_to_deconz(endpoint=endpoint, payload=payload)
         return build_response(data=response, status=200)
     except ClientResponseError as e:
         return build_response(error=f"Failed at: {e.message}", status=e.status)
