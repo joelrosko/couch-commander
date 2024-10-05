8845ecd600a9768176ce3fbbbd252005e4240073
joelrosko <joel.rosko97@gmail.com>
2024-10-05 22:08:24 +0200

    Added 404 and uncaught error handlers (#28)

--- api/src/__init__.py
+++ api/src/__init__.py
@@ -1,11 +1,11 @@
 import os
 from flask import Flask
-from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 from src.config.config import DevConfig, ProdConfig
 from src.routes import api
+from src.utils.error_util import register_error_handlers
 
 def init_server():
     server = Flask(__name__)
@@ -23,5 +23,7 @@ def init_server():
     migrate = Migrate(server, db)
 
     server.register_blueprint(api, url_prefix="/api/v1")
+
+    register_error_handlers(server)
 
     return server, db
