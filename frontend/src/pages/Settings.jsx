cfdcfe97a1a6dcfff8487fd48b0b15e605cd027d
Joel Rosko <joel.rosko97@gmail.com>
2024-10-20 14:58:03 -0700

    Initial settings list

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -1,9 +1,68 @@
 import HeaderBar from "../components/HeaderBar/HeaderBar"
+import HeaderLayout from "../layouts/HeaderLayout"
+import { Box, Divider, List, ListItem, Typography } from "@mui/material"
+import CreateOutlinedIcon from '@mui/icons-material/CreateOutlined';
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+import ArrowForwardIosOutlinedIcon from '@mui/icons-material/ArrowForwardIosOutlined';
+import FormatAlignLeftOutlinedIcon from '@mui/icons-material/FormatAlignLeftOutlined';
 
 const Settings = () => {
+
+  const editName = () => {
+    console.log("edit name")
+  }
+
   return (
     <>
-      <HeaderBar name={'COUCH-COMMANDER'} section={'SETTINGS'} />
+      <HeaderLayout>
+        <HeaderBar name={'COUCH-COMMANDER'} section={'SETTINGS'} />
+      </HeaderLayout>
+      <Box sx={{
+        m: '20px',
+        display: 'flex',
+        justifyContent: 'center',
+        marginBottom: '60px'
+      }}>
+        <Box sx={{width: '100%', maxWidth: '800px'}}>
+          <List sx={{background: '#ffffff', borderRadius: '7px'}}>
+            <ListItem>
+              <Typography variant="body1" sx={{flexGrow: 1}}>
+                Name of house: Vasaplatsen
+              </Typography>
+              <CreateOutlinedIcon onClick={editName} sx={{color: '#d36135', '&:hover': {cursor: 'pointer'}, ml: '8px'}} />
+            </ListItem>
+            <Divider />
+            <ListItem>
+              <Typography variant="body1" sx={{flexGrow: 1}}>
+                Get deConz API key
+              </Typography>
+              <ArrowForwardIosOutlinedIcon onClick={editName} sx={{color: '#d36135', '&:hover': {cursor: 'pointer'}, ml: '8px'}} />
+            </ListItem>
+            <Divider />
+            <ListItem>
+              <Typography variant="body1" sx={{flexGrow: 1}}>
+                Edit lights
+              </Typography>
+              <ArrowForwardIosOutlinedIcon onClick={editName} sx={{color: '#d36135', '&:hover': {cursor: 'pointer'}, ml: '8px'}} />
+            </ListItem>
+            <Divider />
+            <ListItem>
+              <Typography variant="body1" sx={{flexGrow: 1}}>
+                Edit groups
+              </Typography>
+              <ArrowForwardIosOutlinedIcon onClick={editName} sx={{color: '#d36135', '&:hover': {cursor: 'pointer'}, ml: '8px'}} />
+            </ListItem>
+            <Divider />
+            <ListItem>
+              <Typography variant="body1" sx={{flexGrow: 1}}>
+                Logs
+              </Typography>
+              <FormatAlignLeftOutlinedIcon onClick={editName} sx={{color: '#d36135', '&:hover': {cursor: 'pointer'}, ml: '8px'}} />
+              <DeleteOutlineOutlinedIcon onClick={editName} sx={{color: '#d36135', '&:hover': {cursor: 'pointer'}, ml: '8px'}} />
+            </ListItem>
+          </List>
+        </Box>
+      </Box>
     </>
   )
 }
