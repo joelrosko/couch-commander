b0718cf5d453cdc88165171d45f9c9fc2a32faf9
joelrosko <joel.rosko97@gmail.com>
2024-10-30 19:12:32 +0100

    Adding coloring for lights and groups (#77)

--- api/src/utils/data_formatter_util.py
+++ api/src/utils/data_formatter_util.py
@@ -1,17 +1,18 @@
 def format_lights_data(lights:dict):
     updated_response = {}
     for light_id, light_data in lights.items():
+        print(light_data["state"])
         included_ligt_data = {
             "name": light_data["name"],
             "status": light_data["state"]["on"],
             "manufacturer": "IKEA" if light_data["manufacturername"].startswith("IKEA") else light_data["manufacturername"],
             "bri": light_data["state"]["bri"],
             "temprature": light_data["state"]["ct"],
-            "multicolor": True if light_data["state"]["colormode"] == "xy" else False,
+            "multicolor": True if "sat" in light_data["state"] else False,
         }
         if included_ligt_data["multicolor"]:
-            included_ligt_data["color"]  =light_data["state"]["xy"]
-            included_ligt_data["sat"]  =light_data["state"]["sat"]
+            included_ligt_data["color"] = round((light_data["state"]["hue"] / 65535) * 360)
+            included_ligt_data["sat"] = light_data["state"]["sat"]
 
         updated_response[light_id] = included_ligt_data
 
@@ -38,7 +39,7 @@ def format_specific_group_data(group:dict):
         "anyOn": group["state"]['any_on'],
         "bri": group["action"]["bri"],
         "temprature": group["action"]["ct"],
-        "color": group["action"]["xy"],
+        "color": round((group["action"]["hue"] / 65535) * 360),
         "sat": group["action"]["sat"],
         "lights": group["lights"]
     }
